6ec5f240cf6a8761c43e3a431473308f9e266bf9
chrisRhee <0407chan@naver.com>
2020-05-25 16:39:04 +0900

    controller Test

---
+++ BackEnd/src/main/java/com/chris/todoapp/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.chris.todoapp.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
---
+++ BackEnd/src/test/java/com/chris/todoapp/springboot/web/HelloControllerTest.java
@@ -0,0 +1,40 @@
+package com.chris.todoapp.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    public MockMvc mvc;
+
+    @Test
+    public void hello_test() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(hello));
+    }
+
+    @Test
+    public void helloDto_return_test() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        //little test
+//        mvc.perform(MockMvcRequestBuilders.get("/hello/dtd").param("name",name).param("amount",String.valueOf(amount)))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.name", is(name)))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.amount", is(amount)));
+    }
+}
