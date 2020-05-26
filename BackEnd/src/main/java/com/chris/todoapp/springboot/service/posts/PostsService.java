616cdb56070d8f829b1823ea68fcc4e113de6b1c
이찬호 <0407chan@naver.com>
2020-05-26 23:53:05 +0900

    생성 : PostsApiController, 생성 : PostsService 생성 : PostsSaveRequestDto 생성 : PostsApiController 생성 : PostsApiControllerTest
    문제 : PostsApiControllerTest 하려고 하는데 테스트 진행이 안됨. Long 이슈가 나는데 이유를 모르겠다. 찾아봐야겠다.
    110p 까지 진행 완료

---
+++ BackEnd/src/main/java/com/chris/todoapp/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.chris.todoapp.springboot.service.posts;
+
+
+import com.chris.todoapp.springboot.domain.posts.PostsRepository;
+import com.chris.todoapp.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
